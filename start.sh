015381bc443c4b5e8a348a657505ba2b5d6deaf6
rupin <rupin.chheda@gmail.com>
2017-03-01 18:47:25 +0530

    Refresh Requirements

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 script_full_path=$(dirname "$0")
 git pull
+pip install -r requirements.txt
 python  "$script_full_path/index.py" &
 iceweasel 127.0.0.1:8080  &
-#@midori -p -e Fullscreen -a http://127.0.0.1:8080
+
