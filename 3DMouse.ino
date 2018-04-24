1fb1382fb603f3bd5bac7a7ba5442fa540237eef
RupinC <rupin@curiositygym.com>
2018-04-24 12:21:44 +0530

    Update 3DMouse.ino

--- 3DMouse.ino
+++ 3DMouse.ino
@@ -1,20 +1,16 @@
+#include <Mouse.h>
 void setup() {
   pinMode(5, INPUT);
 
 }
 
 void loop() {
-   if(digitalRead(5) == HIGH){
-     Mouse.begin();
-     Mouse.press(MOUSE_LEFT);
+   if(digitalRead(5) == HIGH)
+   {
+     Mouse.begin();     
      Mouse.move(1, 1, 0);
-     
-     //Mouse.release(MOUSE_RIGHT);
-   } 
-
-   if(digitalRead(5) == LOW){
-     Mouse.end();     
-     Mouse.release(MOUSE_LEFT);
-   } 
+     Mouse.end();
+     delay(1000);  
+   }  
 
 }
